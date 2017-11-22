fe785d5b1ebfd5fb68251abcefc3cac8f1049954
charlie  lee <ctlee9096@gmail.com>
2017-11-22 04:02:52 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,6 +25,8 @@ class SongsController < ApplicationController
   end
 
   def new
+    if params[:artist_id]&&!Artist.nil?(params[:artist_id])
+      redirect_to artists_path, alert: "Artist not found"
     @song = Song.new(artist_id: params[:artist_id])
   end
 
