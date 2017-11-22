099cf66329e9a484d12e945fc1d783a796799184
charlie  lee <ctlee9096@gmail.com>
2017-11-22 04:08:00 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -44,6 +44,7 @@ class SongsController < ApplicationController
 
   def edit
     if params[:artist_id]
+      artist = Author.find_by(id: params[:author_id])
     @song = Song.find(params[:id])
   end
 
